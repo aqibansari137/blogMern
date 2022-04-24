9d7368e16bc5fd4adc15e60fed9c87dada880471
aqibansari137 <aqibansari137@gmail.com>
2022-04-24 17:29:39 +0530

    make it better

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ connectDb;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(router);
 dotenv.config();
 //for deploy
 
@@ -25,5 +24,6 @@ if ((process.env.NODE_ENV = "production")) {
     });
 }
 
+app.use(router);
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
