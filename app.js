1e0d6f6ee9d0ec322355710d72f0b30961f96336
aqibansari137 <aqibansari137@gmail.com>
2022-04-24 19:05:31 +0530

    make it better

--- app.js
+++ app.js
@@ -6,14 +6,7 @@ const router = require('./router/router')
 
 const app = express();
 connectDb;
-//for deploy
-if ((process.env.NODE_ENV = "production")) {
-    app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
+
 dotenv.config();
 
 app.use(cors());
@@ -24,7 +17,14 @@ app.use(router);
 
 const PORT = process.env.PORT || 8000;
 
-
+//for deploy
+if ((process.env.NODE_ENV = "production")) {
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
